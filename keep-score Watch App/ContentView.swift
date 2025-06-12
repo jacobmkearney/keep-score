0fd56a498a5b94c9f28996953589f656ba068f2d
jacobmkearney <jacobmkearney@gmail.com>
2025-06-12 16:26:58 +0100

    Initial Commit

---
+++ keep-score Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  keep-score Watch App
+//
+//  Created by Jacob Kearney on 12/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ keep-score Watch App/keep_scoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  keep_scoreApp.swift
+//  keep-score Watch App
+//
+//  Created by Jacob Kearney on 12/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct keep_score_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
